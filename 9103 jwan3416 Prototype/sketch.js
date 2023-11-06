e40566d4822876eb623d9b978a60c37730200a92
Jwan3416 <146395365+Jwan3416@users.noreply.github.com>
2023-11-06 21:05:45 +1100

    second Prototype
    commit ‘Keypressed’

--- 9103 jwan3416 Prototype/sketch.js
+++ 9103 jwan3416 Prototype/sketch.js
@@ -1,20 +1,23 @@
-let sideLength = 200; // diameter of circle
-let gap = 10; // space between  circles
-let rows = 5; 
-let cols = 5; 
-let rotationSpeed = 0.01; // Spin speed
-let smallEllipseDiameter = 14; // diameter of small ellipse
-let smallEllipseCount = 12; // Number of small ovals per  circle
-let smallEllipseDistance = 20; // The distance between the small ellipse and the  circle
-let concentricCircles = 4; // number of concentric circles
-let concentricCircleColors = []; 
-let dottedCircles = 3; // The number of concentric dotted rings per layer
+let sideLength = 200; // Diameter of the large circle, 200
+let gap = 10; // Gap between large circles, 10
+let rows = 5; // Number of rows
+let cols = 8; // Number of columns
+let rotationSpeed = 0.01; // Rotation speed, 0.01
+let smallEllipseDiameter = 14; // Diameter of small ellipses, 14
+let smallEllipseCount = 12; // Number of small ellipses per large circle, 12
+let smallEllipseDistance = 20; // Distance of small ellipses from the large circle, 20
+let concentricCircles = 4; // Number of concentric circles, 4
+let concentricCircleColors = []; // Colors for concentric circles
+let dottedCircles = 3; // Number of dashed circles per layer, 3
+let rotateSpeedPressed = 1;
+let timer;
 
-function genRundomColors() {
-    concentricCircleColors = []; //  Change concentric color
-   // Generate random concentric circle color values
+// Generate random colors in a three-dimensional array
+function generateRandomColors() {
+    concentricCircleColors = []; // Reset concentric circle colors
+    // Generate random colors for concentric circles
     for (let i = 0; i < rows; i++) {
-        concentricCircleColors.push([]); 
+        concentricCircleColors.push([]); // Initialize a two-dimensional array
         for (let j = 0; j < cols; j++) {
             concentricCircleColors[i].push([]);
             for (let k = 0; k < concentricCircles; k++) {
@@ -23,37 +26,38 @@ function genRundomColors() {
         }
     }
 }
-//Get contrast color
+
+// Get the contrasting color
 function getContrastColor(hexColor) {
-    let c = color(hexColor); // creat color 
+    let c = color(hexColor); // Create a color
     let r = red(c); // Get the red component
-    let g = green(c); 
-    let b = blue(c); 
-    let contrastR = 255 - r;//Get the red component value of the contrasting color
+    let g = green(c); // Get the green component
+    let b = blue(c); // Get the blue component
+    let contrastR = 255 - r;
     let contrastG = 255 - g;
     let contrastB = 255 - b;
     return color(contrastR, contrastG, contrastB);
 }
-//Draw wave Circle
+
+// Draw a wavy circular shape
 function drawWaveCircle(i, j, k, radius) {
     push();
-    rotate(frameCount / (50.0 / (j + 50))); 
-    // Shape wave Circle
+    rotate(rotateSpeedPressed * frameCount / (50.0 / (j + 50))); // Change the rotation speed based on the circle's index
+    // Draw a wavy circular shape
     beginShape();
     noFill();
-    stroke(getContrastColor(concentricCircleColors[i][j][k])); 
-    strokeWeight(1); 
+    stroke(getContrastColor(concentricCircleColors[i][j][k])); // Set the stroke color
+    strokeWeight(1); // Set the stroke weight
     for (let angle = 0; angle < 360; angle += 0.5) {
-        let r = radius * 0.85 + 12 * sin(80 * angle); 
+        let r = radius * 0.85 + 12 * sin(80 * angle); // Calculate using degrees to compute the sine function
         let x = r * cos(angle);
         let y = r * sin(angle);
         vertex(x, y);
     }
     endShape(CLOSE);
     beginShape();
-    strokeWeight(1); 
     for (let angle = 0; angle < 360; angle += 0.5) {
-        let r = 38 + 8 * sin(60 * angle); 
+        let r = 38 + 8 * sin(60 * angle); // Calculate using degrees to compute the sine function
         let x = r * cos(angle);
         let y = r * sin(angle);
         vertex(x, y);
@@ -61,22 +65,23 @@ function drawWaveCircle(i, j, k, radius) {
     endShape(CLOSE);
     pop();
 }
-//Draw a dotted loop
+
+// Draw dashed circles
 function drawDottedCircle(i, j, k, radius) {
     for (let n = 0; n < dottedCircles && k < concentricCircles - 1; n++) {
         push();
-        rotate(frameCount / (50.0 / (k + 50)));  // Change rotation speed
-        stroke(getContrastColor(concentricCircleColors[i][j][k])); 
-        strokeWeight(8 - k); 
-        noFill(); 
-        // Set the dotted line style and change the dotted line interval according to the circle index
-        drawingContext.setLineDash([5, 10 + k]);
-        ellipse(0, 0, radius * 2 - 10 - 20 * n); 
+        rotate(rotateSpeedPressed * frameCount / (50.0 / (k + 50))); // Change the rotation speed based on the circle's index
+        stroke(getContrastColor(concentricCircleColors[i][j][k])); // Set the stroke color
+        strokeWeight(8 - k); // Set the stroke weight
+        noFill();
+        drawingContext.setLineDash([5, 10 + k]); // Set the dashed line style
+        ellipse(0, 0, radius * 2 - 10 - 20 * n); // Draw a concentric circle with adjusted radius
         pop();
-        drawingContext.setLineDash([]); // Reset dash style
+        drawingContext.setLineDash([]); // Reset the dashed line style
     }
 }
-//Draw small ovals and connected lines
+
+// Draw small ellipses and connecting curves
 function drawSmallEllipses() {
     let smallEllipses = [];
     for (let k = 0; k < smallEllipseCount; k++) {
@@ -85,7 +90,7 @@ function drawSmallEllipses() {
         let y = (sideLength / 2 + smallEllipseDistance) * sin(angle + smallEllipseDistance * 1.5);
         smallEllipses.push({ x, y });
     }
-     // Draw curves between small ellipses
+    // Draw curves between small ellipses
     stroke('#E8670D');
     strokeWeight(3);
     noFill();
@@ -95,55 +100,56 @@ function drawSmallEllipses() {
         curveVertex(smallEllipses[l].x, smallEllipses[l].y);
     }
     endShape(CLOSE);
-    // Draw a small oval
+    // Draw small ellipses
     for (let l = 0; l < smallEllipseCount; l++) {
-        let ellipseRadius = smallEllipseDiameter / 2; 
-      // change a small oval color
+        let ellipseRadius = smallEllipseDiameter / 2; // Radius of the small ellipse
+        // Draw gradient small ellipses
         for (let i = 0; i <= ellipseRadius; i++) {
-            let t = map(i, 0, ellipseRadius, 0, 1); 
-            let gradientColor = lerpColor(color(255), color(0), t); 
-            fill(gradientColor); 
-            noStroke(); 
-            ellipse(smallEllipses[l].x, smallEllipses[l].y, smallEllipseDiameter - i, smallEllipseDiameter - i); 
+            let t = map(i, 0, ellipseRadius, 0, 1); // Map the radius to a value between 0 and 1
+            let gradientColor = lerpColor(color(255), color(0), t); // Get the interpolated color
+            fill(gradientColor); // Set the fill color to the interpolated color
+            noStroke(); // No stroke
+            ellipse(smallEllipses[l].x, smallEllipses[l].y, smallEllipseDiameter - i, smallEllipseDiameter - i); // Draw a small ellipse
         }
     }
 }
 
-//Draw a gradient semi-arc connecting the centers of two large circles
+// Draw gradient arcs connecting the centers of two large circles
 function drawGradientArc2() {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             push();
-            translate((j + 0.5 * (i % 2)) * (sideLength + gap * 4), i * (sideLength + gap) + sideLength / 2); 
+            translate((j + 0.5 * (i % 2)) * (sideLength + gap * 4), i * (sideLength + gap) + sideLength / 2); // Move the origin to the center of each large circle
             if (!(i % 2 == 0 && j % 2 != 0) && !(i % 2 != 0 && j % 2 == 0)) {
-                let arcStartAngle = 180; 
-                let arcEndAngle = 360; 
+                let arcStartAngle = 180; // Starting angle of the arc
+                let arcEndAngle = 360; // Ending angle of the arc
                 for (let i = arcStartAngle; i <= arcEndAngle; i++) {
-                    let t = map(i, arcStartAngle, arcEndAngle, 0, 1); 
-                    let gradientColor = lerpColor(color(0, 255, 0), color(255, 0, 0), t); 
-                    stroke(gradientColor);
-                    strokeWeight(5); 
-                    noFill(); 
-                    arc(sideLength - gap * 8, 0, sideLength + gap * 4, sideLength * 1, i, i + 1); 
+                    let t = map(i, arcStartAngle, arcEndAngle, 0, 1); // Map the angle to a value between 0 and 1
+                    let gradientColor = lerpColor(color(0, 255, 0), color(255, 0, 0), t); // Get the interpolated color
+                    stroke(gradientColor); // Set the stroke color to the interpolated color
+                    strokeWeight(5); // Set the stroke weight
+                    noFill(); // No fill
+                    arc(sideLength - gap * 8, 0, sideLength + gap * 4, sideLength * 1, i, i + 1); // Draw a small segment of the arc
                 }
             }
             pop();
         }
     }
 }
-//Draw a ConcentricCircles
+
+// Draw concentric circles
 function drawConcentricCircles() {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             push();
-            translate((j + 0.5 * (i % 2)) * (sideLength + gap * 4), i * (sideLength + gap) + sideLength / 2); // Move the origin to the center of each great circle
-            rotate(frameCount * rotationSpeed); 
-        
+            translate((j + 0.5 * (i % 2)) * (sideLength + gap * 4), i * (sideLength + gap) + sideLength / 2); // Move the origin to the center of each large circle
+            rotate(frameCount * rotationSpeed * rotateSpeedPressed); // Make each large circle spin
+            // Draw concentric circles
             for (let k = 0; k < concentricCircles; k++) {
                 let radius = sideLength / 2 - k * (sideLength / (1.6 * concentricCircles));
-                fill(concentricCircleColors[i][j][k]); 
-                noStroke(); 
-                ellipse(0, 0, radius * 2, radius * 2); 
+                fill(concentricCircleColors[i][j][k]); // Set the fill color to a random color
+                noStroke(); // No stroke
+                ellipse(0, 0, radius * 2, radius * 2); // Draw a concentric circle
                 if (i % 2 == 0 && j % 2 != 0) {
                     drawWaveCircle(i, j, k, radius);
                 } else {
@@ -151,7 +157,6 @@ function drawConcentricCircles() {
                 }
             }
             drawSmallEllipses();
-            
             pop();
         }
     }
@@ -160,38 +165,65 @@ function drawConcentricCircles() {
 
 function setup() {
     createCanvas(800, 800);
-    angleMode(DEGREES); // Change angle mode to degrees
-    genRundomColors(); // Generate random color values
-    // creat new start
-    bgColor = color(255,0,0);
+    angleMode(DEGREES); // Change the angle mode to degrees
+    generateRandomColors(); // Generate random colors
+    // Start of new code
+    bgColor = color(255, 0, 0);
     setInterval(changeColor, 2000);
     timer = setTimeout(() => {
-        genRandomColors(); // Randomly generate colors every 5 seconds
+        generateRandomColors(); // Generate random colors every 5 seconds
         timer = setTimeout(arguments.callee, 5000);
-      }, 5000);
-    // creat end start
+    }, 5000);
+    // End of new code
 }
 
 function draw() {
     background(bgColor);
     drawConcentricCircles();
+    fill(255, 255, 255);
+    rect(0, 0, 800, 80);
+    fill(0);
+    textSize(20);
+    textAlign(LEFT, TOP);
+    text("Press left arrow counterclockwise. Press the right arrow clockwise.", 10, 10);
+    text("Press down arrow to slow rotation. Press up arrow to stop rotation.", 10, 40);
 }
 
-// creat new start
-function changeColor(){
+// Start of new code
+function changeColor() {
     let r = random(255);
     let g = random(255);
     let b = random(255);
-    bgColor = color(r,g,b);
+    bgColor = color(r, g, b);
 }
-// creat end start
+// End of new code
 
 function mousePressed() {
-    console.log("test");
     clearTimeout(timer);
-    genRundomColors(); // Instantly generate random colors on mouse click
+    generateRandomColors(); // Generate random colors immediately on mouse click
     timer = setTimeout(() => {
-      genRundomColors(); // Randomly generate colors every 5 seconds
-      timer = setTimeout(arguments.callee, 5000);
+        generateRandomColors(); // Generate random colors every 5 seconds
+        timer = setTimeout(arguments.callee, 5000);
     }, 5000);
-  }
+}
+
+// Reference: p5js.org/reference/#/p5/keyPressed
+function keyPressed() {
+    if (keyCode == LEFT_ARROW) {
+        console.log("LEFT_ARROW");
+        rotateSpeedPressed = -400;
+        console.log(rotateSpeedPressed);
+    } else if (keyCode == RIGHT_ARROW) {
+        console.log("RIGHT_ARROW");
+        rotateSpeedPressed = 400;
+        console.log(rotateSpeedPressed);
+    } else if (keyCode == DOWN_ARROW) {
+        console.log("DOWN_ARROW");
+        rotateSpeedPressed = 10;
+        console.log(rotateSpeedPressed);
+    } else if (keyCode == UP_ARROW) {
+        console.log("UP_ARROW");
+        rotateSpeedPressed = 0;
+        console.log(rotateSpeedPressed);
+    }
+}
